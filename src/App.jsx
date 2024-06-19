546a487fb954783505f5df9b1317a3f66e7dde99
JessieF-2021 <jessieffianu@gmail.com>
2024-06-19 17:12:25 +0000

    color picker app

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css';
+import ColorPicker from './ColorPicker';
+function App() {
+
+
+  return (
+    <>
+    <div className="app">
+     
+<ColorPicker/>
+    </div>
+    </>
+  )
+}
+
+export default App
